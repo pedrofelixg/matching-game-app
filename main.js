b4b43716fece3c4b55b44e9f7383af550ae53ac2
pedrofelixg <pedro.viniciusacm@gmail.com>
2023-05-16 21:29:49 -0300

    :sparkles: Create more Card Game and Player Name

--- main.js
+++ main.js
@@ -3,8 +3,18 @@ import "./src/styles/generic/reset.css"
 import "./src/styles/elements/base.css";
 
 import CardGame from "./src/components/CardGame";
+import playerName from "./src/components/PlayerScore";
 
 const $root = document.querySelector("#root");
 const $htmlCardGame = CardGame();
+const $playerNameInsert = playerName();
 
-$root.insertAdjacentHTML("beforeend", $htmlCardGame);
+// $root.insertAdjacentHTML("beforeend", $htmlCardGame);
+$root.insertAdjacentHTML("beforeend", $playerNameInsert);
+function insertCard () {
+    for (let i = 0; i < 6; i++) {
+        $root.insertAdjacentHTML("beforeend", $htmlCardGame);
+    }
+}
+
+insertCard();
---
+++ src/components/PlayerScore/index.js
@@ -0,0 +1,14 @@
+import "../CardGame/style.css";
+
+function playerName() {
+    return `
+        <h2 class="player-name">
+            Player 1
+        </h2>
+        <h2 class="player-name">
+            Player 2
+        </h2>
+    `
+}
+// pretendo tornar isso personalizado para o usuário;
+export default playerName;
